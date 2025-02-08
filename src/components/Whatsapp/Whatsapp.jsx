8804683a0476cca6a4e24740b34af810fc0106df
MAbdullahAbbasi <officialabdullah261@gmail.com>
2025-02-09 04:51:41 +0500

    Little Modification of Links 1

--- src/components/Whatsapp/Whatsapp.jsx
+++ src/components/Whatsapp/Whatsapp.jsx
@@ -9,32 +9,19 @@ const WhatsApp = () => {
     <> 
         <div className="whatsapp-container">
 
-        <a
-            href="https://mail.google.com/mail/?view=cm&fs=1&to=engineerabdullah020@gmail.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={gmailLogo}
-              alt="Email"
-              className="gmail-image"
-              width={80}
-            />
+          
+          <a
+              href="https://wa.me/923456265572?text=Hello%20Abdullah!%20I'm%20interested%20in%20your%20services."
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={whatsappImage}
+                alt="WhatsApp"
+                className="whatsapp-image"
+                width={80}
+              />
           </a>
-        <a
-            href="https://wa.me/923456265572?text=Hello%20Abdullah!%20I'm%20interested%20in%20your%20services."
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img
-              src={whatsappImage}
-              alt="WhatsApp"
-              className="whatsapp-image"
-              width={80}
-            />
-          </a>
-
-
           <a
             href="https://pk.linkedin.com/in/muhammad-abdullah-abbasi-417719288"
             target="_blank"
@@ -45,6 +32,18 @@ const WhatsApp = () => {
               alt="LinkedIn"
               className="linkedin-image"
             />
+          </a>
+          <a
+              href="https://mail.google.com/mail/?view=cm&fs=1&to=engineerabdullah020@gmail.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img
+                src={gmailLogo}
+                alt="Email"
+                className="gmail-image"
+                width={80}
+              />
           </a>
 
           
