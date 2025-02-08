682add2eb2fb1834696bdaf4ff13bf3e755ee49f
MAbdullahAbbasi <officialabdullah261@gmail.com>
2025-02-08 19:34:37 +0500

    Whatsapp Working

--- src/components/Whatsapp/Whatsapp.jsx
+++ src/components/Whatsapp/Whatsapp.jsx
@@ -4,21 +4,22 @@ import '../../styling/Whatsapp/Whatsapp.css'
 
 const WhatsApp = () => {
   return (
-    <>
+    <> 
         <div className="whatsapp-container">
-            <a
-            href="https://wa.me/yourwhatsappphonenumber" 
-            target="_blank"
-            rel="noopener noreferrer"
-            className="fixed bottom-4 right-4 z-50"
-            >
-            <img
-                src={whatsappImage}
-                alt="WhatsApp"
-                className="whatsapp-image"
-                width={80}
-            />
-            </a>
+        <a
+          href="https://wa.me/923456265572?text=Hello%20Abdullah!%20I'm%20interested%20in%20your%20services."
+          target="_blank"
+          rel="noopener noreferrer"
+          className="fixed bottom-4 right-4 z-50"
+        >
+          <img
+            src={whatsappImage}
+            alt="WhatsApp"
+            className="whatsapp-image"
+            width={80}
+          />
+        </a>
+
         </div>
     </>
   );
